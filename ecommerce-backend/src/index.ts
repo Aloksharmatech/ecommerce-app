987051ddc2ac57348065a4d68e219c06ebcf90eb
aloksharmatech <sharma.alok.tech@gmail.com>
2025-07-13 20:31:50 +0530

    client url

--- ecommerce-backend/src/index.ts
+++ ecommerce-backend/src/index.ts
@@ -6,7 +6,12 @@ import productRoutes from "./routes/product.routes";
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin:"https://ecommerce-app-1-ce5j.onrender.com",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/uploads", express.static("uploads")); // serve images
 app.use("/api/products", productRoutes);

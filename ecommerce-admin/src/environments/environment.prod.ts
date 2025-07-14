4651f02452fbd148f04be8d1b26d8b7da65a5ee6
aloksharmatech <sharma.alok.tech@gmail.com>
2025-07-14 19:24:04 +0530

    changed

--- ecommerce-admin/src/environments/environment.prod.ts
+++ ecommerce-admin/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ecommerce-app-4bnj.onrender.com/api', // replace with actual Render URL
+  apiUrl: 'https://ecommerce-app-4bnj.onrender.com/api', 
 };
